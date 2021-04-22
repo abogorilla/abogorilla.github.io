627986013b25f39f8d357d72af61e7a07dda3761
abogorilla <75017088+abogorilla@users.noreply.github.com>
2021-04-22 08:59:45 +0200

    python

---
+++ bitbyte.py
@@ -0,0 +1,21 @@
+bit= float(input("Kor mange bit har du? "))
+print ("Du har",bit,"bit.")
+kb= bit / 1000
+mb= bit / 1000000
+gb= bit / 1000000000
+KB= bit / 8000
+MB= bit / 8000000
+GB= bit / 8000000000
+print("Dette er",kb,"Kb.")
+
+print("Dette er",mb,"Mb.")
+
+print("Dette er",gb,"Gb.")
+
+print("Dette er",KB,"KB.")
+
+print("Dette er",MB,"MB.")
+
+print("Dette er",GB,"GB")
+
+print("Du oppgav eit tal med ", len(str(int(bit))), "siffer.")
