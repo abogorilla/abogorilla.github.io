8e97a5a389b1de4ae703b1a48a71d1aa51243586
abogorilla <75017088+abogorilla@users.noreply.github.com>
2021-04-23 09:55:51 +0200

    fagdag

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,5 +1,15 @@
-var bilmerke = [Volvo, Mercedes, Audi, Tesla, Toyota];
+//Oppgave 1 //
+var bilmerke = ["Volvo", "Mercedes", "Audi", "Tesla", "Toyota"];
 bilmerke.push("Peugeot");
+bilmerke.length;
+console.log(bilmerke[2]);
 
-console.log(bilmerke[1].length)
+//Oppgave 2 //
+var arrey = [3,5,7,10,20];
+arrey.length;
+console.log (arrey [0] + arrey [4]);
+console.log (arrey [0] + arrey [1] + arrey [2] + arrey [3] + arrey [4]);
+console.log(arrey[3]);
 
+//Oppgave 3 //
+var tall1 = [1,2,3,4,5,6,7,8,9,10];
---
+++ rock paper scissor.js
@@ -0,0 +1,7 @@
+var man = 0;
+var comp = 0;
+
+function buttonClicked(buttonClicked) {
+    var results = ["rock", "paper", "scissor"];
+    var rand = Math.floor(Math.random()*3);
+}
